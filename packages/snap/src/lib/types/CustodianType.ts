2beffd5a1fa86fdc60b59486d579fbfa046596f9
Shane Terence Odlum <shane.odlum@consensys.net>
2024-12-10 13:06:16 +0000

    chore: enable bitgo and cactus

--- packages/snap/src/lib/types/CustodianType.ts
+++ packages/snap/src/lib/types/CustodianType.ts
@@ -1,14 +1,13 @@
+import { BitgoCustodianApi } from '../custodian-types/bitgo/BitgoCustodianApi';
+import { CactusCustodianApi } from '../custodian-types/cactus/CactusCustodianApi';
 import { ECA1CustodianApi } from '../custodian-types/eca1/ECA1CustodianApi';
 import { ECA3CustodianApi } from '../custodian-types/eca3/ECA3CustodianApi';
-
-// import { BitgoCustodianApi } from "../custodian-types/bitgo/BitgoCustodianApi";
-// import { CactusCustodianApi } from "../custodian-types/cactus/CactusCustodianApi";
 
 export enum CustodianType {
   ECA3 = 'ECA3',
   ECA1 = 'ECA1',
-  // BitGo = 'BitGo',
-  // Cactus = 'Cactus',
+  BitGo = 'BitGo',
+  Cactus = 'Cactus',
 }
 
 // Commenting out the broken custodians for now
@@ -16,6 +15,6 @@ export enum CustodianType {
 export const CustodianApiMap = {
   [CustodianType.ECA3]: ECA3CustodianApi,
   [CustodianType.ECA1]: ECA1CustodianApi,
-  // [CustodianType.BitGo]: BitgoCustodianApi,
-  // [CustodianType.Cactus]: CactusCustodianApi,
+  [CustodianType.BitGo]: BitgoCustodianApi,
+  [CustodianType.Cactus]: CactusCustodianApi,
 };
