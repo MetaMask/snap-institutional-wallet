a6a8a5edb5ad7510978832c1843d265fb90604a6
John Renner <j@cubist.dev>
2026-01-27 12:30:52 -0500

    Update cubist data for onboarding

--- packages/snap/src/lib/custodian-types/custodianMetadata.ts
+++ packages/snap/src/lib/custodian-types/custodianMetadata.ts
@@ -368,20 +368,20 @@ export const custodianMetadata: (
     iconUrl:
       'https://assets-global.website-files.com/638a2693daaf8527290065a3/651802cf8d04ec5f1a09ce86_Logo.svg',
     isManualTokenInputSupported: true,
-    allowedOnboardingDomains: [], // Cubist does not support onboarding via a web page
+    allowedOnboardingDomains: ['app-gamma.signer.cubist.dev'],
   },
   {
-    refreshTokenUrl: 'https://dg5z0qnzb9s65.cloudfront.net/v0/oauth/token',
-    name: 'cubist-test',
-    displayName: 'Cubist Test',
+    refreshTokenUrl: 'https://beta.signer.cubist.dev/v0/oauth/token',
+    name: 'cubist-beta',
+    displayName: 'Cubist Beta',
     production: false,
-    apiBaseUrl: 'https://dg5z0qnzb9s65.cloudfront.net/v0/mmi',
+    apiBaseUrl: 'https://beta.signer.cubist.dev/v0/mmi',
     apiVersion: CustodianType.ECA3,
     custodianPublishesTransaction: false,
     iconUrl:
       'https://assets-global.website-files.com/638a2693daaf8527290065a3/651802cf8d04ec5f1a09ce86_Logo.svg',
     isManualTokenInputSupported: true,
-    allowedOnboardingDomains: [], // Cubist does not support onboarding via a web page
+    allowedOnboardingDomains: ['app-beta.signer.cubist.dev', 'localhost:3000'],
   },
   {
     refreshTokenUrl: 'https://prod.signer.cubist.dev/v0/oauth/token',
@@ -394,7 +394,7 @@ export const custodianMetadata: (
     iconUrl:
       'https://assets-global.website-files.com/638a2693daaf8527290065a3/651802cf8d04ec5f1a09ce86_Logo.svg',
     isManualTokenInputSupported: true,
-    allowedOnboardingDomains: [], // Cubist does not support onboarding via a web page
+    allowedOnboardingDomains: ['app.signer.cubist.dev'],
   },
   {
     refreshTokenUrl: 'http://localhost:3330/oauth/token',
